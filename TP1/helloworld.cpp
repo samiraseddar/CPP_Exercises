20fde6861c94a394f50812b49163815a1b2c08df
samiraseddar <seddar20191@gmail.com>
2023-01-12 15:57:24 +0100

    la derniere chaine de caracter

--- TP1/helloworld.cpp
+++ TP1/helloworld.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 int main(int argc, char const *argv[])
 {
-    int var;
+    std::string var;
+    std::cout << "What's your name?" << std::endl;
     std::cin >> var;
+    std::cout << "Hello !" << var << std::endl;
     return 0;
 }
