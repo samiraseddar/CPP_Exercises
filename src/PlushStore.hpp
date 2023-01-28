4b868b52f47e99b14f40083e994713169ce0fb03
samiraseddar <seddar20191@gmail.com>
2023-01-28 22:31:12 +0100

    j'ai rajouter

--- src/PlushStore.hpp
+++ src/PlushStore.hpp
@@ -10,7 +10,11 @@ public:
     {
         return name;
     }
-    int get_wealth_amount() const
+    int get_stock_size()
+    {
+        return size;
+    }
+    float get_wealth_amount() const
     {
         return argent;
     }
@@ -18,9 +22,39 @@ public:
     {
         return stock;
     }
+    void loan(const float &a)
+    {
+        argent += a;
+    }
+    int get_experience()
+    {
+        return size;
+    }
+    float make_plush(const float &a)
+    {
+        return
+    }
+    void make_plush(const float &a)
+    {
+        if (argent > 0)
+        {
+            size++;
+            argent = argent - a;
+        }
+        if (argent < 0)
+        {
+            argent = 0;
+        }
+    }
+    float get_debt_amount()
+    {
+        return argent + (argent * 0.10);
+        ;
+    }
 
 private:
     std::string name;
-    int argent = 0;
+    float argent = 0.0;
     int stock = 0;
+    int size = 0;
 };
--- tests/test-08.cpp
+++ tests/test-08.cpp
@@ -5,12 +5,12 @@
 TEST_CASE("Test 08 - "
           "Les magasins de peluches peuvent emprunter de l'argent a 10 pour cent d'interets.")
 {
-    auto plush_store = PlushStore { "Not That Broke Anymore" };
+    auto plush_store = PlushStore{"Not That Broke Anymore"};
 
     plush_store.loan(100);
     REQUIRE(plush_store.get_wealth_amount() == 100);
     REQUIRE(plush_store.get_debt_amount() == 110);
-    
+
     plush_store.loan(300);
     REQUIRE(plush_store.get_wealth_amount() == 400);
     REQUIRE(plush_store.get_debt_amount() == 440);
