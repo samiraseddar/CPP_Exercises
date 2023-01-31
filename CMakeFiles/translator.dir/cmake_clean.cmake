68d9a6a6ad809050b837360c4721f6a1d63b6416
samiraseddar <seddar20191@gmail.com>
2023-01-31 18:46:15 +0100

    j'ai finis le tp2

---
+++ CMakeFiles/translator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/translator.dir/src/translator.cpp.o"
+  "translator"
+  "translator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/translator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/translator.cpp
+++ src/translator.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <map>
 #include <sstream>
+#include <fstream>
 std::set<std::string> make_exit_commands()
 {
 
@@ -16,16 +17,31 @@ void print(const std::map<std::string, std::string> &dic)
         std::cout << s.first << " " << s.second << std::endl;
     }
 }
-int main()
+int main(int argc, char const *argv[])
 {
+    std::string src;
+    std::string cible;
+    if (argc == 3)
+    {
+        src = argv[1];
+        cible = argv[2];
+    }
+    else
+    {
+        src = "fr";
+        cible = "en";
+    }
     std::string x;
     std::map<std::string, std::string> mapp;
+    std::vector<std::string> history;
 
     while (true)
     {
+        std::cout << src << " " << cible << std::endl;
         std::cout << "This is a translator, but it does nothing for now..." << std::endl;
         std::string s;
         getline(std::cin, x);
+        const std::string xx = x;
         std::stringstream stream(x);
         std::vector<std::string> tab;
         if (make_exit_commands().count(x) != 0)
@@ -36,8 +52,39 @@ int main()
         {
             tab.emplace_back(s);
         }
+        std::cout << xx << std::endl;
+        if (tab[0] == "load")
+        {
+            std::string ss;
+            std::ifstream fichier(tab[1]);
+            std::vector<std::string> tab2;
+            while (fichier >> s)
+            {
+                std::stringstream ss(s);
+
+                while (ss >> s)
+                {
+                    std::cout << s << std::endl;
+                    tab2.emplace_back(s);
+                }
+            }
+            fichier.close();
+        }
+        if (tab[0] == "save")
+        {
+            std::ofstream fichier(tab[1]);
+
+            for (auto ss : history)
+            {
+
+                fichier << ss << std::endl;
+            }
+            fichier.close();
+        }
         if (tab[0] == "add")
         {
+            history.emplace_back(xx);
+
             if (tab.size() == 3)
             {
                 mapp.emplace(tab[1], tab[2]);
@@ -51,9 +98,11 @@ int main()
                 }
             }
         }
+
         if (tab[0] == "translate")
         {
-            for (int i = 1; i < tab.size(); i++)
+            int siz = tab.size();
+            for (int i = 1; i < siz; i++)
             {
                 if (mapp.count(tab[i]) != 0)
                 {
@@ -64,6 +113,14 @@ int main()
                     std::cout << "???" << std::endl;
                 }
             }
+        }
+        if (tab[0] == "clear")
+        {
+            mapp.clear();
+        }
+        if (tab[0] == "remove")
+        {
+            mapp.erase(tab[1]);
         }
     }
 
