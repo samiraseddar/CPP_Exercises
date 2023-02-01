5bbd93713127101adb5f2b58aea95c8936c88afc
samiraseddar <seddar20191@gmail.com>
2023-02-01 21:01:41 +0100

    tp2

--- src/translator.cpp
+++ src/translator.cpp
@@ -1,126 +1,107 @@
 #include <iostream>
 #include <string>
-#include <set>
-#include <vector>
-#include <map>
 #include <sstream>
+#include <set>
+#include <map>
+#include <vector>
 #include <fstream>
 std::set<std::string> make_exit_commands()
 {
-
     return {"q", "quit", "e", "exit"};
 }
 void print(const std::map<std::string, std::string> &dic)
 {
     for (auto s : dic)
     {
-        std::cout << s.first << " " << s.second << std::endl;
+        std::cout << s.first << "  " << s.second << std::endl;
     }
 }
 int main(int argc, char const *argv[])
 {
-    std::string src;
-    std::string cible;
-    if (argc == 3)
-    {
-        src = argv[1];
-        cible = argv[2];
-    }
-    else
-    {
-        src = "fr";
-        cible = "en";
-    }
-    std::string x;
-    std::map<std::string, std::string> mapp;
-    std::vector<std::string> history;
+    /* code */
+    return 0;
+}
 
+int main()
+{
+    std::string line;
+    std::map<std::string, std::string> dic;
+    std::vector<std::string> history;
     while (true)
     {
-        std::cout << src << " " << cible << std::endl;
-        std::cout << "This is a translator, but it does nothing for now..." << std::endl;
-        std::string s;
-        getline(std::cin, x);
-        const std::string xx = x;
-        std::stringstream stream(x);
         std::vector<std::string> tab;
-        if (make_exit_commands().count(x) != 0)
+        std::cout << "This is a translator, but it does nothing for now..." << std::endl;
+        getline(std::cin, line);
+        std::stringstream stream(line);
+        std::string s;
+        if (make_exit_commands().count(line) != 0)
         {
-            return 0;
+            return 1;
         }
         while (stream >> s)
         {
             tab.emplace_back(s);
         }
-        std::cout << xx << std::endl;
-        if (tab[0] == "load")
-        {
-            std::string ss;
-            std::ifstream fichier(tab[1]);
-            std::vector<std::string> tab2;
-            while (fichier >> s)
-            {
-                std::stringstream ss(s);
-
-                while (ss >> s)
-                {
-                    std::cout << s << std::endl;
-                    tab2.emplace_back(s);
-                }
-            }
-            fichier.close();
-        }
-        if (tab[0] == "save")
-        {
-            std::ofstream fichier(tab[1]);
-
-            for (auto ss : history)
-            {
-
-                fichier << ss << std::endl;
-            }
-            fichier.close();
-        }
         if (tab[0] == "add")
         {
-            history.emplace_back(xx);
-
+            history.emplace_back(line);
             if (tab.size() == 3)
             {
-                mapp.emplace(tab[1], tab[2]);
-                std::cout << tab[1] << " => " << tab[2] << std::endl;
+                dic[tab[1]] = tab[2];
             }
             else
             {
-                if (mapp.count(tab[1]))
+                if (dic.count(tab[1]) != 0)
                 {
-                    std::cout << tab[1] << " => " << mapp[tab[1]] << std::endl;
-                }
-            }
-        }
-
-        if (tab[0] == "translate")
-        {
-            int siz = tab.size();
-            for (int i = 1; i < siz; i++)
-            {
-                if (mapp.count(tab[i]) != 0)
-                {
-                    std::cout << mapp[tab[i]] << std::endl;
+                    std::cout << tab[1] << " => " << dic[tab[1]] << std::endl;
                 }
                 else
                 {
-                    std::cout << "???" << std::endl;
+                    std::cout << "????" << std::endl;
                 }
             }
         }
-        if (tab[0] == "clear")
+        if (tab[0] == "print")
         {
-            mapp.clear();
+            print(dic);
         }
-        if (tab[0] == "remove")
+        if (tab[0] == "save")
         {
-            mapp.erase(tab[1]);
+            std::ofstream file(tab[1]);
+            for (auto s : history)
+            {
+                file << s << std::endl;
+            }
+        }
+        if (tab[0] == "load")
+        {
+            std::ifstream file(tab[1]);
+
+            while (file >> s)
+            {
+                std::stringstream str(s);
+                std::vector<std::string> tab2;
+                std::string valeur;
+                while (str >> valeur)
+                {
+                    tab2.emplace_back(valeur);
+                }
+                if (tab2.size() == 3)
+                {
+                    dic[tab2[1]] = tab2[2];
+                }
+                if(tab2.size()==2)
+                {
+                    if (dic.count(tab2[1]) != 0)
+                    {
+                        std::cout << tab2[1] << " => " << dic[tab2[1]] << std::endl;
+                    }
+                    else
+                    {
+                        std::cout << "????" << std::endl;
+                    }
+                }
+            }
         }
     }
 
