e08c12e7f928f986863f5e078a6f3d7909a9acea
samiraseddar <seddar20191@gmail.com>
2023-03-03 07:46:16 +0100

    dd

--- tests/Test-2.cpp
+++ tests/Test-2.cpp
@@ -4,12 +4,12 @@
 
 TEST_CASE("2a. Pokeballs are empty by default")
 {
-    auto pokeball = Pokeball {};
+    auto pokeball = Pokeball{};
     REQUIRE(pokeball.empty() == true);
 }
 
 TEST_CASE("2b. Querying whether a Pokeball is empty does not modify the Pokeball")
 {
-    const auto pokeball = Pokeball {};
+    const auto pokeball = Pokeball{};
     REQUIRE(pokeball.empty() == true);
 }
